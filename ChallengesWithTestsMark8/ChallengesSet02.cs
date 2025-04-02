078d369aced74a1dabbfb3fac16d9e72f3d52b03
Jeff Brown <jeff@jeffbrown.tech>
2025-04-02 06:38:58 -0700

    Completed Weekly Challenge Set 2

--- ChallengesWithTestsMark8/ChallengesSet02.cs
+++ ChallengesWithTestsMark8/ChallengesSet02.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace ChallengesWithTestsMark8
 {
@@ -7,52 +8,110 @@ namespace ChallengesWithTestsMark8
     {
         public bool CharacterIsALetter(char c)
         {
-            throw new NotImplementedException();
+            return char.IsLetter(c);
+        }
+
+        public bool CharacterIsNotALetter(char c)
+        {
+            return !char.IsLetter(c);
         }
 
         public bool CountOfElementsIsEven(string[] vals)
         {
-            throw new NotImplementedException();
+            return vals.Length % 2 == 0;
         }
 
         public bool IsNumberEven(int number)
         {
-            throw new NotImplementedException();
+            return number % 2 == 0;
         }
 
         public bool IsNumberOdd(int num)
         {
-            throw new NotImplementedException();
+            return num % 2 != 0;
         }
 
         public double SumOfMinAndMax(IEnumerable<double> numbers)
         {
-            throw new NotImplementedException();
+
+            if (numbers != null && numbers.Any())
+            {
+
+                double max = numbers.Max();
+                double min = numbers.Min();
+                return max + min;
+            }
+            else
+            {
+                return 0;
+            }
         }
 
         public int GetLengthOfShortestString(string str1, string str2)
         {
-            throw new NotImplementedException();
+            return str1.Length > str2.Length ? str2.Length : str1.Length;
         }
 
         public int Sum(int[] numbers)
         {
-            throw new NotImplementedException();
+            int returnSum = 0;
+
+            if (numbers != null)
+            {
+                foreach (int num in numbers)
+                {
+                    returnSum += num;
+                }
+            }
+
+            return returnSum;
         }
 
         public int SumEvens(int[] numbers)
         {
-            throw new NotImplementedException();
+            int evenSum = 0;
+
+            if (numbers != null)
+            {
+                foreach (int num in numbers)
+                {
+                    if ((num % 2) == 0)
+                    {
+                        evenSum += num;
+                    }
+                }
+            }
+
+            return evenSum;
         }
 
         public bool IsSumOdd(List<int> numbers)
         {
-            throw new NotImplementedException();
+            int returnSum = 0;
+
+            if (numbers != null)
+            {
+                foreach (int num in numbers)
+                {
+                    returnSum += num;
+                }
+            }
+
+            return returnSum % 2 != 0 ? true : false;
         }
 
         public long CountOfPositiveOddsBelowNumber(long number)
         {
-            throw new NotImplementedException();
+            long count = 0;
+            for (long i = number - 1; i > 0; i--)
+            {
+                if (i % 2 != 0)
+                {
+                    count++;
+                }
+            }
+
+            return count;
         }
     }
 }
